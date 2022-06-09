9cb9f259df31b8293df9af2fcbc5da434b50fb91
Kristina Strakh <kris.strakg@gmail.com>
2022-06-09 11:35:00 +0300

    Task 3

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ else
 44 5 78 -> 78
 22 3 9 -> 22*/
 
-Console.Write("Введите первое число: ");
+/*Console.Write("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
@@ -42,4 +42,22 @@ if (max < c)
 {
    max = c;
 }
- Console.WriteLine ("Максимальное число:" + max);
+ Console.WriteLine ("Максимальное число:" + max);*/
+
+
+ /*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет*/
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2==0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
