1fe9ac438fc094d4c770ecb23f9e32bbde655a6b
Kristina Strakh <kris.strakg@gmail.com>
2022-06-09 11:51:40 +0300

    Task 4

--- Program.cs
+++ Program.cs
@@ -49,7 +49,9 @@ if (max < c)
 4 -> да
 -3 -> нет
 7 -> нет*/
-Console.Clear();
+
+
+/*Console.Clear();
 Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
@@ -60,4 +62,21 @@ if (a%2==0)
 else
 {
     Console.WriteLine("No");
+}*/
+
+/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 2;
+
+while(sum<N)
+
+{
+    Console.Write (sum + " ");
+    sum+=2;
 }
+
